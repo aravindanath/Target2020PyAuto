1c505bfa3d73227fb5100e252f2c30bc4bb9c91c
aravindanath <aravindanath86@gmail.com>
2020-02-29 16:55:43 +0530

    selenium methods

--- browserLaunch/launchChrome.py
+++ browserLaunch/launchChrome.py
@@ -1,10 +1,11 @@
-from selenium import webdriver
+
 from selenium.webdriver import Chrome,ChromeOptions
-import time
 
 path = "../driver/chromedriver"
 
 ops = ChromeOptions()
+ops.add_argument('--ignore-certificate-errors')
 ops.add_argument("--disable-notifications")
 driver = Chrome(executable_path=path,options=ops)
 driver.fullscreen_window()
+driver.implicitly_wait(30)
